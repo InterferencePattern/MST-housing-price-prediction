824ec9fbfea47d55bcc1f0efbb47bf9987919683
James Budarz <jimbudarz@gmail.com>
2018-08-26 18:51:39 -0400

    Fixed bug in plotting predictions

--- step4-stack.py
+++ step4-stack.py
@@ -16,8 +16,10 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
 import numpy as np
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def rmsle(y_pred, y_test) :
+def rmsle(y_pred, y_test):
     assert len(y_test) == len(y_pred)
     return np.sqrt(np.mean((np.log(y_pred) - np.log(y_test))**2))
 
@@ -28,6 +30,7 @@ def rmsle(y_pred, y_test) :
 # |    |                   |                   |                   |        |
 # |    |                   |                   |                   |        |
 # |    |                   |                   |                   |        |
+
 
 # Load training and test predictions
 rows_to_drop = ['model1', 'model2', 'model3',
@@ -66,24 +69,26 @@ test_predictions = all_predictions[all_predictions.index > 1460]
 
 # Grid search for multiple hyperparameters:
 boostModel = GradientBoostingRegressor()
-grid_param = [{'max_depth': range(1, 4),
+grid_param = [{'max_depth': range(1, 3),
                'n_estimators': range(10, 500, 100),
-               'learning_rate': np.linspace(.01,.1,2)}]
+               'learning_rate': np.linspace(.01, .1, 10)}]
 boostModel.set_params(random_state=7)
 para_search = GridSearchCV(estimator=boostModel,
                            param_grid=grid_param,
                            scoring=None,
-                           cv=5, n_jobs=7,
+                           cv=10, n_jobs=7,
                            return_train_score=True,
                            verbose=1)
 para_search = para_search.fit(train_predictions, actualPrices)
 bestModel = para_search.best_estimator_
+print(bestModel)
 
 # Fit the best model to the test data
 bestModel.fit(train_predictions, actualPrices)
-predictedTest = pd.Series(bestModel.predict(test_predictions)).apply(np.exp)
-predictedTrain = pd.Series(bestModel.predict(train_predictions)).apply(np.exp)
+metapredictedTest = pd.Series(bestModel.predict(test_predictions)).apply(np.exp)
+metapredictedTrain = pd.Series(bestModel.predict(train_predictions)).apply(np.exp)
 
+print(metapredictedTest)
 ############################
 # Save CSV in Kaggle format
 ############################
@@ -91,7 +96,7 @@ predictedTrain = pd.Series(bestModel.predict(train_predictions)).apply(np.exp)
 outputFileName = 'stacked_predictions.csv'
 
 # Make a list of test data IDs and Prices
-id_price = list(zip(range(1461, 2920), predictedTest))
+id_price = list(zip(range(1461, 2920), metapredictedTest))
 
 # Rounds price to 1 decimal place (as per sample submission example)
 id_price = list(map(lambda x: [x[0], round(x[1], 1)], id_price))
@@ -104,16 +109,45 @@ final_df.to_csv(outputFileName, index=False)
 
 print('Saved all predictions as ' + outputFileName)
 
-####################
-# Print performance
-####################
+#################################################
+# Print performance and graphs for various models
+#################################################
+
+clr = "#3C1E3F"
+sns.set(font_scale=1.25, style="darkgrid")
+plt.style.use('ggplot')
+plt.figure(figsize=(16,16))
+sns.set_context('talk')
 
 for model in all_predictions.columns:
-    currentPredictions = all_predictions[model][:1457]
+    currentPredictions = train_predictions[model]
     print("For Model " + str(model))
-    print("Root Mean Squared Error: $" + str(np.sqrt(mean_squared_error(y_pred=currentPredictions.apply(np.exp), y_true=actualPrices.apply(np.exp)))))
-    print("Log Score: " + str(rmsle(currentPredictions.apply(np.exp), actualPrices.apply(np.exp))))
+    print("Root Mean Squared Error: $" + str(np.sqrt(mean_squared_error(
+        y_pred=currentPredictions.apply(np.exp), y_true=actualPrices.apply(np.exp)))))
+    print("Log Score: " + str(rmsle(currentPredictions.apply(np.exp),
+                                    actualPrices.apply(np.exp))))
+    grid = sns.scatterplot(x = actualPrices.apply(np.exp)/1000, y = currentPredictions.apply(np.exp)/1000, color=clr)
+    plt.plot([0, 1000], [0, 1000], linewidth=2)
+    plt.title(model)
+    plt.xlabel("Actual price (USD/1000)")
+    plt.ylabel("Predicted price (USD/1000)")
+    plt.ticklabel_format(style='plain', axis='y')
+    plt.tight_layout()
+    plt.savefig('images/' + model + 'performance.png', bbox_inches='tight', dpi=300)
+    plt.clf()
 
+# Print results from the metamodel
+actualPrices = np.exp(np.array(actualPrices))
 print('For metamodel:')
-print("Root Mean Squared Error: $" + str(np.sqrt(mean_squared_error(y_pred=predictedTrain, y_true=actualPrices.apply(np.exp)))))
-print("Log Score: " + str(rmsle(y_pred = predictedTrain, y_test = actualPrices.apply(np.exp))))
+print("Root Mean Squared Error: $" +
+      str(np.sqrt(mean_squared_error(y_pred = metapredictedTrain, y_true = actualPrices))))
+print("Log Score: " + str(rmsle(y_pred = metapredictedTrain, y_test = actualPrices)))
+
+grid = sns.scatterplot(x = actualPrices/1000, y = metapredictedTrain/1000, color=clr)
+plt.plot([0, 1000], [0, 1000], linewidth=2)
+plt.title('Stacked Metamodel Performance')
+plt.xlabel("Actual price (USD/1000)")
+plt.ylabel("Predicted price (USD/1000)")
+plt.ticklabel_format(style='plain', axis='y')
+plt.tight_layout()
+plt.savefig('images/metamodelperformance.png', bbox_inches='tight', dpi=300)
