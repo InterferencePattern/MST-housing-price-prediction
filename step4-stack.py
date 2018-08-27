ee72fae56d415752dd4d9165517e4156ae0f9237
James Budarz <jimbudarz@gmail.com>
2018-08-26 22:35:59 -0400

    Edited plotting

--- step4-stack.py
+++ step4-stack.py
@@ -20,6 +20,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import cross_val_score
 
+
 def rmsle(y_pred, y_test):
     assert len(y_test) == len(y_pred)
     return np.sqrt(np.mean((np.log(y_pred) - np.log(y_test))**2))
@@ -37,28 +38,34 @@ def rmsle(y_pred, y_test):
 rows_to_drop = ['model1', 'model2', 'model3',
                 'model4', 'model5', 'model6', 'actual']
 
+rstacked_df = pd.read_csv('R-StackedResults.csv', index_col='Id').apply(np.log)
+rstacked_df.columns = ["R Predictions"]
+
 linear_df = pd.read_csv('linear_results.csv',
                         index_col='Id').drop(rows_to_drop, axis=1)
-linear_df.columns = ["LinearPredictions"]
+linear_df.columns = ["Multivariate Linear"]
 
 gb_df = pd.read_csv('gb_results.csv', index_col='Id').drop(
     rows_to_drop, axis=1)
-gb_df.columns = ["GBoostPredictions"]
+gb_df.columns = ["Gradient Boost"]
 
 xgb_df = pd.read_csv('xgb_results.csv', index_col='Id').drop(
     rows_to_drop, axis=1)
-xgb_df.columns = ["XGBoostPredictions"]
+xgb_df.columns = ["XGBoost"]
 
 rf1_df = pd.read_csv('rf1_results.csv', index_col='Id').drop(
     rows_to_drop, axis=1)
-rf1_df.columns = ["RandForest1Predictions"]
+rf1_df.columns = ["Optimized Random Forest"]
 
 rf2_df = pd.read_csv('rf2_results.csv', index_col='Id').drop(
     rows_to_drop, axis=1)
-rf2_df.columns = ["RandForest2Predictions"]
+rf2_df.columns = ["Default Random Forest"]
 
 all_predictions = linear_df.merge(gb_df, on='Id', how='inner').merge(
-    xgb_df, on='Id', how='inner').merge(rf1_df, on='Id', how='inner').merge(rf2_df, on='Id', how='inner')
+    xgb_df, on='Id', how='inner').merge(
+    rf1_df, on='Id', how='inner').merge(
+    rf2_df, on='Id', how='inner')#.merge(
+    # rstacked_df, on='Id', how='inner')
 
 actualPrices = pd.read_csv('linear_results.csv', index_col='Id')['actual']
 
@@ -86,8 +93,10 @@ print(bestModel)
 
 # Fit the best model to the test data
 bestModel.fit(train_predictions, actualPrices)
-metapredictedTest = pd.Series(bestModel.predict(test_predictions)).apply(np.exp)
-metapredictedTrain = pd.Series(bestModel.predict(train_predictions)).apply(np.exp)
+metapredictedTest = pd.Series(
+    bestModel.predict(test_predictions)).apply(np.exp)
+metapredictedTrain = pd.Series(
+    bestModel.predict(train_predictions)).apply(np.exp)
 
 ############################
 # Save CSV in Kaggle format
@@ -112,36 +121,41 @@ print('Saved all predictions as ' + outputFileName)
 #################################################
 # Print performance and graphs for various models
 #################################################
+actualLogPrices = actualPrices
+actualPrices = np.exp(np.array(actualPrices))
 
 clr = "#3C1E3F"
-sns.set(font_scale=1.25, style="darkgrid")
+sns.set(style="darkgrid")
 plt.style.use('ggplot')
-plt.figure(figsize=(16,10))
-sns.set_context('talk')
+plt.figure(figsize=(16, 10))
+sns.set_context('talk', font_scale=1)
 
 for model in all_predictions.columns:
     currentPredictions = train_predictions[model]
     print("For Model " + str(model))
     print("Root Mean Squared Error: $" + str(np.sqrt(mean_squared_error(
-        y_pred=currentPredictions.apply(np.exp), y_true=actualPrices.apply(np.exp)))))
+        y_pred=currentPredictions.apply(np.exp), y_true=actualLogPrices.apply(np.exp)))))
     print("Log Score: " + str(rmsle(currentPredictions.apply(np.exp),
-                                    actualPrices.apply(np.exp))))
-    grid = sns.scatterplot(x = actualPrices.apply(np.exp)/1000, y = currentPredictions.apply(np.exp)/1000, color=clr)
-    plt.plot([0, 1000], [0, 1000], linewidth=2)
+                                    actualLogPrices.apply(np.exp))))
+    grid = sns.scatterplot(x=actualLogPrices.apply(
+        np.exp) / 1000, y=currentPredictions.apply(np.exp) / 1000, color=clr)
+    plt.plot([0, 800], [0, 800], linewidth=2)
     plt.title(model)
     plt.xlabel("Actual price (USD/1000)")
     plt.ylabel("Predicted price (USD/1000)")
     plt.ticklabel_format(style='plain', axis='y')
     plt.tight_layout()
-    plt.savefig('images/' + model + 'performance.png', bbox_inches='tight', dpi=300)
+    plt.savefig('images/' + model + 'performance.png',
+                bbox_inches='tight', dpi=300)
     plt.clf()
 
 # Print results from the metamodel
-print("Metamodel gives CV score of " + str(np.sqrt(-np.mean(cross_val_score(bestModel, train_predictions, actualPrices, cv=5, scoring = 'neg_mean_squared_error')))))
+print("Metamodel gives CV score of " + str(np.sqrt(-np.mean(cross_val_score(bestModel,
+                                                                            train_predictions, actualPrices, cv=5, scoring='neg_mean_squared_error')))))
 
-actualPrices = np.exp(np.array(actualPrices))
-grid = sns.scatterplot(x = actualPrices/1000, y = metapredictedTrain/1000, color=clr)
-plt.plot([0, 1000], [0, 1000], linewidth=2)
+grid = sns.scatterplot(x=actualPrices / 1000,
+                       y=metapredictedTrain / 1000, color=clr)
+plt.plot([0, 800], [0, 800], linewidth=2)
 plt.title('Stacked Metamodel Performance')
 plt.xlabel("Actual price (USD/1000)")
 plt.ylabel("Predicted price (USD/1000)")
