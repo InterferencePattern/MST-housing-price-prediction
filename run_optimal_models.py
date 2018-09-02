9785d855d5ecf950514f82da438dad833e585a88
James Budarz <jimbudarz@gmail.com>
2018-09-02 17:34:38 -0400

    Converted redundant code in run_optimal_models.py to a single function

--- run_optimal_models.py
+++ run_optimal_models.py
@@ -31,6 +31,30 @@ from xgboost import XGBRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import cross_val_score
 
+def run_a_model(modelObject):
+    # Data frame to store results
+    current_results = pd.DataFrame({"Id":train_prices.index, "actual":train_prices.log_SalePrice})
+    current_results.set_index('Id',inplace=True)
+    # iterate through the 5 folds to create the first 5 models and gen results
+    for i in range(0,5):
+        modelObject.fit(train.iloc[train_indicies[i],], train_prices.iloc[train_indicies[i],].values)  # model on 80%
+        pred = modelObject.predict(train.iloc[test_indicies[i]])                              # predict on 20%
+        pred = [i for i in pred]
+        model_name = "model" + str(i+1)
+        temp_df = pd.DataFrame({'Id':train.index[test_indicies[i]], model_name:pred})    # temp DF with ID and new model results
+        temp_df.set_index('Id', inplace=True)
+        current_results = current_results.merge(temp_df, on='Id', how='left', left_index = True) # add to current results DF
+    # create a 6th model - train on all training, predict on all test ('to_guess')
+    modelObject.fit(train, train_prices.values)
+    pred = modelObject.predict(to_guess)
+    pred = [i for i in pred]
+    temp_df = pd.DataFrame({'Id':to_guess.index, "model6":pred})
+    temp_df.set_index('Id', inplace=True)
+    current_results = current_results.merge(temp_df, on='Id', how='outer') # add to current_results DF
+    score = str(np.sqrt(-np.mean(cross_val_score(modelObject, train, train_prices, cv=5, scoring = 'neg_mean_squared_error'))))
+    current_results['predictions'] = current_results[['model1','model2','model3','model4','model5','model6']].mean(axis = 1)
+    return current_results, score
+
 ##### Load the data #####
 # Training data - has a saleprice column
 train = pd.read_csv(sys.argv[1])
@@ -57,8 +81,7 @@ except:
 try:
     with open('rf1_params.pkl', 'rb') as f:
         param_dict_rf1 = pickle.load(f)
-
-        model_rf1 = RandomForestRegressor(**param_dict_rf1)
+    model_rf1 = RandomForestRegressor(**param_dict_rf1)
 except:
     print('Warning: Running Random Forest 1 with default hyperparameters')
     model_rf1 = RandomForestRegressor()
@@ -67,7 +90,6 @@ except:
 try:
     with open('rf2_params.pkl', 'rb') as f:
         param_dict_rf2 = pickle.load(f)
-
     model_rf2 = RandomForestRegressor(**param_dict_rf2)
 except:
     print('Warning: Running Random Forest 2 with default hyperparameters')
@@ -77,7 +99,6 @@ except:
 try:
     with open('xgb_params.pkl', 'rb') as f:
         param_dict_xgb = pickle.load(f)
-
     model_xgb = XGBRegressor(**param_dict_xgb)
 except:
     print('Warning: Running XGBoost with default hyperparameters')
@@ -87,7 +108,6 @@ except:
 try:
     with open('gb_params.pkl', 'rb') as f:
         param_dict_gb = pickle.load(f)
-
     model_gb = GradientBoostingRegressor(**param_dict_gb)
 except:
     print('Warning: Running Gradient Boosting with default hyperparameters')
@@ -109,161 +129,32 @@ for train_index, test_index in kf.split(train):
 # train_prices.iloc[test_indicies[0]].shape <-- first 20% test labels
 
 ###### Linear Regression #####
-# Data frame to store results
-linear_results = pd.DataFrame({"Id":train_prices.index, "actual":train_prices.log_SalePrice})
-linear_results.set_index('Id',inplace=True)
-
-# iterate through the 5 folds to create the first 5 models and gen results
-for i in range(0,5):
-    model_linear.fit(train.iloc[train_indicies[i],], train_prices.iloc[train_indicies[i],])  # model on 80%
-    pred = model_linear.predict(train.iloc[test_indicies[i]])                              # predict on 20%
-    pred = [i[0] for i in pred]
-    model_name = "model" + str(i+1)
-    temp_df = pd.DataFrame({'Id':train.index[test_indicies[i]], model_name:pred})    # temp DF with ID and new model results
-    temp_df.set_index('Id', inplace=True)
-    linear_results = linear_results.merge(temp_df, on='Id', how='left', left_index = True) # add to linear_results DF
-
-# create a 6th model - train on all training, predict on all test ('to_guess')
-model_linear.fit(train, train_prices)
-pred = model_linear.predict(to_guess)
-pred = [i[0] for i in pred]
-temp_df = pd.DataFrame({'Id':to_guess.index, "model6":pred})
-temp_df.set_index('Id', inplace=True)
-linear_results = linear_results.merge(temp_df, on='Id', how='outer') # add to linear_results DF
-print("Linear gives CV score of " + str(np.sqrt(-np.mean(cross_val_score(model_linear, train, train_prices, cv=5, scoring = 'neg_mean_squared_error')))))
-
-# reorder cols (actual at the end)
-#linear_results = linear_results[['model1','model2','model3','model4','model5','model6','actual']]
-linear_results['predictions'] = linear_results[['model1','model2','model3','model4','model5','model6']].mean(axis = 1)
-
+linear_results, score = run_a_model(model_linear)
+print("Linear model gives CV score of " + str(score))
 # output to a csv
 linear_results.to_csv('linear_results.csv')
 
 ###### Random Forest 1 #####
-# Data frame to store results
-rf1_results = pd.DataFrame({"Id":train_prices.index, "actual":train_prices.log_SalePrice})
-rf1_results.set_index('Id',inplace=True)
-
-# iterate through the 5 folds to create the first 5 models and gen results
-for i in range(0,5):
-    model_rf1.fit(train.iloc[train_indicies[i]], train_prices.iloc[train_indicies[i]].log_SalePrice)  # model on 80%
-    pred = model_rf1.predict(train.iloc[test_indicies[i]])                              # predict on 20%
-    pred = [i for i in pred]
-    model_name = "model" + str(i+1)
-    temp_df = pd.DataFrame({'Id':train.index[test_indicies[i]], model_name:pred})    # temp DF with ID and new model results
-    temp_df.set_index('Id', inplace=True)
-    rf1_results = rf1_results.merge(temp_df, on='Id', how='left') # add to rf1_results DF
-
-# create a 6th model - train on all training, predict on all test ('to_guess')
-model_rf1.fit(train, train_prices.log_SalePrice)
-pred = model_rf1.predict(to_guess)
-pred = [i for i in pred]
-temp_df = pd.DataFrame({'Id':to_guess.index, "model6":pred})
-temp_df.set_index('Id', inplace=True)
-rf1_results = rf1_results.merge(temp_df, on='Id', how='outer') # add to rf1_results DF
-print("RF1 gives CV score of " + str(np.sqrt(-np.mean(cross_val_score(model_rf1, train, train_prices, cv=5, scoring = 'neg_mean_squared_error')))))
-
-
-# # reorder cols (actual at the end)
-#rf1_results = rf1_results[['model1','model2','model3','model4','model5','model6','actual']]
-rf1_results['predictions'] = rf1_results[['model1','model2','model3','model4','model5','model6']].mean(axis = 1)
-
+rf1_results, score = run_a_model(model_rf1)
+print("Random forest 1 gives CV score of " + str(score))
 # # output to a csv
 rf1_results.to_csv('rf1_results.csv')
 
 ##### Random Forest 2 #####
-# Data frame to store results
-rf2_results = pd.DataFrame({"Id":train_prices.index, "actual":train_prices.log_SalePrice})
-rf2_results.set_index('Id',inplace=True)
-
-# iterate through the 5 folds to create the first 5 models and gen results
-for i in range(0,5):
-    model_rf2.fit(train.iloc[train_indicies[i]], train_prices.iloc[train_indicies[i]].log_SalePrice)  # model on 80%
-    pred = model_rf2.predict(train.iloc[test_indicies[i]])                              # predict on 20%
-    pred = [i for i in pred]
-    model_name = "model" + str(i+1)
-    temp_df = pd.DataFrame({'Id':train.index[test_indicies[i]], model_name:pred})    # temp DF with ID and new model results
-    temp_df.set_index('Id', inplace=True)
-    rf2_results = rf2_results.merge(temp_df, on='Id', how='left') # add to rf2_results DF
-
-# create a 6th model - train on all training, predict on all test ('to_guess')
-model_rf2.fit(train, train_prices.log_SalePrice)
-pred = model_rf2.predict(to_guess)
-pred = [i for i in pred]
-temp_df = pd.DataFrame({'Id':to_guess.index, "model6":pred})
-temp_df.set_index('Id', inplace=True)
-rf2_results = rf2_results.merge(temp_df, on='Id', how='outer') # add to rf2_results DF
-print("RF2 gives CV score of " + str(np.sqrt(-np.mean(cross_val_score(model_rf2, train, train_prices, cv=5, scoring = 'neg_mean_squared_error')))))
-
-
-# # reorder cols (actual at the end)
-#rf2_results = rf2_results[['model1','model2','model3','model4','model5','model6','actual']]
-rf2_results['predictions'] = rf2_results[['model1','model2','model3','model4','model5','model6']].mean(axis = 1)
-
-
+rf2_results, score = run_a_model(model_rf2)
+print("Random forest 2 gives CV score of " + str(score))
 # # output to a csv
 rf2_results.to_csv('rf2_results.csv')
 
 ##### XGBoost #####
 # Data frame to store results
-xgb_results = pd.DataFrame({"Id":train_prices.index, "actual":train_prices.log_SalePrice})
-xgb_results.set_index('Id',inplace=True)
-
-# iterate through the 5 folds to create the first 5 models and gen results
-for i in range(0,5):
-    model_xgb.fit(train.iloc[train_indicies[i]], train_prices.iloc[train_indicies[i]].log_SalePrice)  # model on 80%
-    pred = model_xgb.predict(train.iloc[test_indicies[i]])                              # predict on 20%
-    pred = [i for i in pred]
-    model_name = "model" + str(i+1)
-    temp_df = pd.DataFrame({'Id':train.index[test_indicies[i]], model_name:pred})    # temp DF with ID and new model results
-    temp_df.set_index('Id', inplace=True)
-    xgb_results = xgb_results.merge(temp_df, on='Id', how='left') # add to xgb_results DF
-
-# create a 6th model - train on all training, predict on all test ('to_guess')
-model_xgb.fit(train, train_prices.log_SalePrice)
-pred = model_xgb.predict(to_guess)
-pred = [i for i in pred]
-temp_df = pd.DataFrame({'Id':to_guess.index, "model6":pred})
-temp_df.set_index('Id', inplace=True)
-xgb_results = xgb_results.merge(temp_df, on='Id', how='outer') # add to xgb_results DF
-print("XGB gives CV score of " + str(np.sqrt(-np.mean(cross_val_score(model_xgb, train, train_prices, cv=5, scoring = 'neg_mean_squared_error')))))
-
-# # reorder cols (actual at the end)
-#xgb_results = xgb_results[['model1','model2','model3','model4','model5','model6','actual']]
-xgb_results['predictions'] = xgb_results[['model1','model2','model3','model4','model5','model6']].mean(axis = 1)
-
-
+xgb_results, score = run_a_model(model_xgb)
+print("XGBoost gives CV score of " + str(score))
 # # output to a csv
 xgb_results.to_csv('xgb_results.csv')
 
 ##### GradientBoost
-# Data frame to store results
-gb_results = pd.DataFrame({"Id":train_prices.index, "actual":train_prices.log_SalePrice})
-gb_results.set_index('Id',inplace=True)
-
-# iterate through the 5 folds to create the first 5 models and gen results
-for i in range(0,5):
-    model_gb.fit(train.iloc[train_indicies[i]], train_prices.iloc[train_indicies[i]].log_SalePrice)  # model on 80%
-    pred = model_gb.predict(train.iloc[test_indicies[i]])                              # predict on 20%
-    pred = [i for i in pred]
-    model_name = "model" + str(i+1)
-    temp_df = pd.DataFrame({'Id':train.index[test_indicies[i]], model_name:pred})    # temp DF with ID and new model results
-    temp_df.set_index('Id', inplace=True)
-    gb_results = gb_results.merge(temp_df, on='Id', how='left') # add to gb_results DF
-
-# create a 6th model - train on all training, predict on all test ('to_guess')
-model_gb.fit(train, train_prices.log_SalePrice)
-pred = model_gb.predict(to_guess)
-pred = [i for i in pred]
-temp_df = pd.DataFrame({'Id':to_guess.index, "model6":pred})
-temp_df.set_index('Id', inplace=True)
-gb_results = gb_results.merge(temp_df, on='Id', how='outer') # add to gb_results DF
-print("GB gives CV score of " + str(np.sqrt(-np.mean(cross_val_score(model_gb, train, train_prices, cv=5, scoring = 'neg_mean_squared_error')))))
-
-# # reorder cols (actual at the end)
-#gb_results = gb_results[['model1','model2','model3','model4','model5','model6','actual']]
-gb_results['predictions'] = gb_results[['model1','model2','model3','model4','model5','model6']].mean(axis = 1)
-
-
+gb_results, score = run_a_model(model_gb)
+print("Gradient Boost gives CV score of " + str(score))
 # # output to a csv
 gb_results.to_csv('gb_results.csv')
